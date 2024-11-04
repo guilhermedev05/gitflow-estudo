6cbdced7f62589f59df20b7b83fc50cfbe012282
Guilherme Santana <163065225+guilhermedev05@users.noreply.github.com>
2024-11-04 13:23:54 -0300

    fix: funcao soma corrigido

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const soma = (a,b) => a+b
+const soma = (a,b) => {
+    return a + b // Bug corrigido
+}
 const subtracao = (a,b) => a-b
 const multiplicacao = (a,b) => a*b
 const divisao = (a,b) => a/b
